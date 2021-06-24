4fe023c22f09461ea07ab15d3a6c8fc24536f593
kamran <kamranasim0@gmail.com>
2021-06-24 23:44:33 +0400

    int float boolean

--- src/Echo.java
+++ src/Echo.java
@@ -1,5 +1,8 @@
 public class Echo {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int age = 18;
+        float length = 78.0332F;
+        boolean baby = true;
+
     }
 }
